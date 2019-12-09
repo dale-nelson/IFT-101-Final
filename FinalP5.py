045e51578691b526879387c2ee8912dbf8f8db13
dale-nelson <codingdev.dale@gmail.com>
2019-12-08 22:40:47 -0700

    Fixed the size of a line

--- FinalP5.py
+++ FinalP5.py
@@ -1,4 +1,5 @@
-orig_string = "I like to go dancing in the rain, but rain should not be too heavy"
+orig_string = "I like to go dancing in the rain,\
+but rain should not be too heavy"
 
 orig_string = orig_string.replace("dancing","driving")
 orig_string = orig_string.replace("rain","forest")
