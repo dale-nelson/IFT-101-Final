89d0eaa9b15b062d29fc8e678581e5d609fc071d
dale-nelson <codingdev.dale@gmail.com>
2019-12-08 17:02:03 -0700

    Completed code for Part 5

---
+++ FinalP5.py
@@ -0,0 +1,7 @@
+orig_string = "I like to go dancing in the rain, but rain should not be too heavy"
+
+orig_string = orig_string.replace("dancing","driving")
+orig_string = orig_string.replace("rain","forest")
+orig_string = orig_string.replace("heavy","long")
+
+print(orig_string)
